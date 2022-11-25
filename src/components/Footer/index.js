dd593563a87b99bba3483e3d851ce5e5d447ca4f
Rossi Kamal <rossikamal@gmail.com>
2022-11-26 04:42:49 +0600

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,10 +8,10 @@ function Footer() {
 		<div className='footer-container'>
 			<section className='footer-subscription'>
 				<p className='footer-subscription-heading'>
-					Join the Adventure newsletter to receive our best vacation deals
+					Join and share your exciting videos.
 				</p>
 				<p className='footer-subscription-text'>
-					You can unsubscribe at any time.
+					You are paid for your videos.
 				</p>
 				<div className='input-areas'>
 					<form>
@@ -28,7 +28,7 @@ function Footer() {
 			<div class='footer-links'>
 				<div className='footer-link-wrapper'>
 					<div class='footer-link-items'>
-						<h2>About Us</h2>
+						<h2>Service</h2>
 						<Link to='/sign-up'>How it works</Link>
 						<Link to='/'>Testimonials</Link>
 						<Link to='/'>Careers</Link>
@@ -36,7 +36,7 @@ function Footer() {
 						<Link to='/'>Terms of Service</Link>
 					</div>
 					<div class='footer-link-items'>
-						<h2>Contact Us</h2>
+						<h2>Product</h2>
 						<Link to='/'>Contact</Link>
 						<Link to='/'>Support</Link>
 						<Link to='/'>Destinations</Link>
@@ -45,14 +45,14 @@ function Footer() {
 				</div>
 				<div className='footer-link-wrapper'>
 					<div class='footer-link-items'>
-						<h2>Videos</h2>
+						<h2>Social Media</h2>
 						<Link to='/'>Submit Video</Link>
 						<Link to='/'>Ambassadors</Link>
 						<Link to='/'>Agency</Link>
 						<Link to='/'>Influencer</Link>
 					</div>
 					<div class='footer-link-items'>
-						<h2>Social Media</h2>
+						<h2>About Us</h2>
 						<Link to='/'>Instagram</Link>
 						<Link to='/'>Facebook</Link>
 						<Link to='/'>Youtube</Link>
@@ -68,7 +68,7 @@ function Footer() {
 							<i class='fab fa-typo3' />
 						</Link>
 					</div>
-					<small class='website-rights'>TRVL © 2020</small>
+					<small class='website-rights'>EZStream © 2022</small>
 					<div class='social-icons'>
 						<Link
 							class='social-icon-link facebook'
